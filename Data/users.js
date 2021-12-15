866f2088b21c7dc78eaca8facdc3b5bbc05bdf8e
AwuduBarikisu <barikisuawudu11@gmail.com>
2021-12-15 08:18:45 -0800

    my first commit

---
+++ Data/users.js
@@ -0,0 +1,8 @@
+let users = [
+    { fname: "Barikisu", lname: "awudu", dob: "11/05/1998", school: "UDS" },
+    { fname: "Rashidah", lname: "Rashid", dob: "23/06/2002", school: "Nyankpala UDS" },
+    { fname: "Millicent", lname: "Awudor", dob: "11/11/2000", school: "Legon" },
+  ];
+  module.exports = users;
+  
+  
---
+++ README.md
@@ -0,0 +1,10 @@
+# userwebservice
+
+This web service is use to manage users. It allows to create and fetch all users.
+
+## Tools
+
+express
+morgan
+
+
